82622054043c6d8a6bb2c578392750318d7bedc6
texrich2002 <60866652+texrich2002@users.noreply.github.com>
2020-12-07 12:17:19 -0600

    funcion sumar pares

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
-export default class App {}
-
+export default class App {
+sumarParesFor(){
+    let i,suma=0;
+    for(i=1; i<=20; i++){
+        if(i % 2 == 0){
+            console.log(i);
+            suma += i;
+        }
+        
+    }
+    console.log("La suma de los pares es: ");
+    return suma;
+}
+}
 let app = new App();
+console.log("Funcion sumar los pares de 1-20");
+console.log(app.sumarParesFor());
+
