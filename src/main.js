a8e2fce8719959e78c915e9f5402cd02b03e5556
texrich2002 <60866652+texrich2002@users.noreply.github.com>
2020-12-07 14:32:05 -0600

    for estaba mal hecho

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
+export default class App {
+    sumarParesFor(){
+        let i,suma=0;
+        for(i=0; i<=20; i+=2){
+            console.log(i);
+            suma += i;
+        }
+        console.log("La suma de los pares es: ");
+        return suma;
+    }
 
+    contarImparesFor(inicio, fin){
+        let i,t,suma=0;
+        if(inicio > fin){
+            t = inicio;
+            inicio = fin;
+            fin = t;
+        }
+        for(i=inicio; i<=fin; i++){
+            if(i % 2 != 0){
+            console.log(i);
+            suma += i;
+            }    
+        }
+        console.log("La suma de los impares es: ");
+        return suma;
+        }
+
+}
+let app = new App();
+console.log("Funcion sumar los pares de 1-20:");
+console.log(app.sumarParesFor());
+console.log("Funcion sumar los impares de un rango:");
+console.log(app.contarImparesFor(1,20));
